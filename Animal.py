bff1d6f7f0e799edc7b8ea2e7899398892535bfb
jorgeplatero <jorge.platero@outlook.com>
2025-09-20 20:37:05 -0300

    atualizando dados

--- Animal.py
+++
@@ -1,8 +0,0 @@
-class Animal:
-    def __init__(self):
-        self.nome = ''
-        self.idade = 0
-        self.especie = ''
-
-    def desc(self):
-        return f'Nome: {self.nome}, Idade: {self.idade} anos, Espécie: {self.especie}'
